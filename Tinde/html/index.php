93680d79bec44d7946d93718335ffbd5a9f49c3c
Profix007 <mats_guttormsen@hotmail.com>
2018-03-12 22:30:02 +0100

    Phpscript nav/footer funker
    Phpscript nav/footer funker

--- Tinde/html/index.php
+++ Tinde/html/index.php
@@ -33,42 +33,7 @@
 <body>
     
     <!--main nav start-->
-    <nav class=" custom navbar  navbar-default  navbar-fixed-top">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header ">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-        <a class="navbar-brand custom" href="#">
-        <img class="brandimg" src="logo.bmp" alt="wtf"></a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav ">
-      
-        <li class="right"><a href="#">Kontakt oss</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle right" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Våre områder<span class="caret"></span></a>
-             <ul class="dropdown-menu">
-            <li><a href="#">Lillehammer</a></li>
-            <li><a href="#">Hedmark</a></li>
-            <li><a href="#">Oppland</a></li>
-                <li><a href="#">Syvin</a></li>
-            <li><a href="#">Sjerka</a></li>
-          </ul>
-        </li>
-        <!--MORE LIST ITEMS  <li ><a href="#">'</a></li>-->
-      </ul>
-   
-      
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+    <?php include("header.html"); ?>
     <script>// Animations init
 new WOW().init();                    
                     </script>
@@ -230,7 +195,7 @@ new WOW().init();
         <!--END ABT cont-->
     </div>
     <!--END about-->
-      <?php include("header.html"); ?>
+      <?php include("footer.html"); ?>
     <div>
         <!--START remove hideable button
         <button class="ShowLess"><img src= "http://icons.iconarchive.com/icons/iconsmind/outline/48/Arrow-Up-icon.png"></button></div>
